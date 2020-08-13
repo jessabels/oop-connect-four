d0feed3148b7952a21a371e71e73e50220ae7bd2
Cole McCoy <cole.mccoy41@hotmail.com>
2020-08-13 14:29:55 -0500

    updated new game button

--- connect-four.js
+++ connect-four.js
@@ -1,11 +1,41 @@
 window.addEventListener("DOMContentLoaded", () => {
-  const square0 = document.getElementById("square-0-0");
-  const squareDiv = document.createElement("div");
-  squareDiv.classList.add("token", "black");
-  square0.appendChild(squareDiv);
+    const playerOneName = document.getElementById("player-1-name");
+    const playerTwoName = document.getElementById("player-2-name");
+    const newGame = document.getElementById("new-game");
+    const formHolder = document.getElementById("form-holder");
+    const boardHolder = document.getElementById("board-holder");
+    const gameName = document.getElementById("game-name");
+    let game = undefined;
 
-  const square1 = document.getElementById("square-0-1");
-  const square1Div = document.createElement("div");
-  square1Div.classList.add("token", "red");
-  square1.appendChild(square1Div);
+    function updateUI() {
+        if (game === undefined){
+            boardHolder.classList.add("is-invisible");
+            console.log(game);
+        } else {
+            boardHolder.classList.remove("is-invisible");
+            console.log(game);
+            gameName.innerHTML = Game.getName();
+        }
+    }
+
+    formHolder.addEventListener("keyup", event => {
+        if (playerOneName.value !== "" && playerTwoName.value !== ""){
+            newGame.disabled = false;
+        } else {
+            newGame.disabled = true;
+
+        }
+    });
+
+    newGame.addEventListener("click", event => {
+        game = new Game(playerOneName.value, playerTwoName.value);
+        playerOneName.value = "";
+        playerTwoName.value = "";
+        newGame.disabled = true;
+        updateUI();
+    });
+
+
+
+
 });
